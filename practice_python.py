3d839fa039a5df2f3a4c0b47985d5b1c53546114
Byte With Ishaq <bytewithishaq@gmail.com>
2025-11-21 17:43:17 +0530

    some corrections in practice_python.py

--- practice_python.py
+++ practice_python.py
@@ -1,169 +1,170 @@
-# print("Ishaq is my name", "and soon i am going to be 19")
-# print(99)
-# print(99+1)
+print("Ishaq is my name", "and soon i am going to be 19")
+print(99)
+print(99+1)
 
-# #Variables
+#Variables
 
-# name = "eesharkkkk"
-# age = 18
-# occupation = "Student"
-# marks = 76.77
+name = "eesharkkkk"
+age = 18
+occupation = "Student"
+marks = 76.77
 
-# print("My name is", name, "and my age is", age, "and my occupation is", occupation, "and my marks are", marks)
+print("My name is", name, "and my age is", age, "and my occupation is", occupation, "and my marks are", marks)
 
-# print(name)
+print(name)
 
-# #to check the variable type
+#to check the variable type
 
-# print(type(name))
-# print(type(age))
-# print(type(occupation))
-# print(type(marks))
+print(type(name))
+print(type(age))
+print(type(occupation))
+print(type(marks))
 
-# print('''hshhsh''')
-# print("ishaq")
-# print('ishaqqq')
+print('''hshhsh''')
+print("ishaq")
+print('ishaqqq')
 
-# #Code for sum od 2 numbers
+#Code for sum od 2 numbers
 
-# a = int(input("Enter a number 1: "))
-# b = int(input("Enter a number 2: "))
+a = int(input("Enter a number 1: "))
+b = int(input("Enter a number 2: "))
 
-# sum22 = a + b
-# print(sum22)
+sum22 = a + b
+print(sum22)
 
-# q = 1
-# w = 3
+q = 1
+w = 3
 
-# sum1 = q + w
-# print(sum1)
+sum1 = q + w
+print(sum1)
 
-# #subtraction
+#subtraction
 
-# input1 = int(input("Enter first number :"))
-# input2 = int(input("Enter second number :"))
+input1 = int(input("Enter first number :"))
+input2 = int(input("Enter second number :"))
 
-# sum3 = input1 - input2
-# print(sum3)
+sum3 = input1 - input2
+print(sum3)
 
-# #multiplication
+#multiplication
 
-# input3 = int(input("Enter first number :"))
-# input4 = int(input("Enter second number :"))
+input3 = int(input("Enter first number :"))
+input4 = int(input("Enter second number :"))
 
-# sum4 = input3 * input4
-# print(sum4)
+sum4 = input3 * input4
+print(sum4)
 
-# #divison
+#divison
 
-# input5 = int(input("Enter first number :"))
-# input6 = int(input("Enter second number :"))
+input5 = int(input("Enter first number :"))
+input6 = int(input("Enter second number :"))
 
-# sum5 = input5 / input6
-# print(sum5)
+sum5 = input5 / input6
+print(sum5)
 
 
-# Operators
+Operators
 
-# x = int(input("Enter first number: "))
-# y = int(input("Enter second number: "))
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
 
-# add = x + y
-# print(add)
+add = x + y
+print(add)
 
-# sub = x - y
-# print(sub)
+sub = x - y
+print(sub)
 
-# mul = x * y
-# print(mul)
+mul = x * y
+print(mul)
 
-# div = x / y
-# print(div)
+div = x / y
+print(div)
 
-# #remainder
+#remainder
 
-# module = x % y
-# print(module)
+module = x % y
+print(module)
 
-# #power x^y
+#power x^y
 
-# pow = x ** y
-# print(pow)
+pow = x ** y
+print(pow)
 
-# #RELATIONAL OPERATORS
+#RELATIONAL OPERATORS
 
-# s = 50
-# d = 40
+s = 50
+d = 40
 
-# print(s == d)
-# print(s != d)
-# print(s > d)
-# print(s < d)
-# print(s >= d)
-# print(s <= d)
+print(s == d)
+print(s != d)
+print(s > d)
+print(s < d)
+print(s >= d)
+print(s <= d)
 
-# #assignment operators
+#assignment operators
 
-# num = 20
-# num = num + 10
+num = 20
+num = num + 10
 
-# num += 20
-# num -= 10
-# num *= 2
-# num **= 2
-# print(num)
+num += 20
+num -= 10
+num *= 2
+num **= 2
+print(num)
 
-# #logical operators
+#logical operators
 
-# num1 = 20
-# num2 = 30
+num1 = 20
+num2 = 30
 
-# print(not (num1 > num2))
+print(not (num1 > num2))
 
-# val1 = 200
-# val2 = 150
+val1 = 200
+val2 = 150
 
-# print("ans opearator : ", val1 and val2)
-# print("ans opearator : ", val1 or val2)
+print("ans opearator : ", val1 and val2)
+print("ans opearator : ", val1 or val2)
 
-# print("ans opearator:", (val1 == val2 )) and (val1 != val2)
-# print("ans opearator:", (val1 != val2 )) and (val1 != val2)
+print("ans opearator:", (val1 == val2 )) and (val1 != val2)
+print("ans opearator:", (val1 != val2 )) and (val1 != val2)
 
-# #type casting
+#type casting
 
-# con1 = "10"
-# con2 = 20
+con1 = "10"
+con2 = 20
 
-# con1 = int(con1)
+con1 = int(con1)
 
-# print(con1 + con2)
+print(con1 + con2)
 
-# typecon = int(input("Enter a number :"))
-# print(type(typecon), typecon)
+typecon = int(input("Enter a number :"))
+print(type(typecon), typecon)
 
-# #make a program to get the sum of 2 inputs
+#make a program to get the sum of 2 inputs
 
-# first = int(input("Enter first number :"))
-# second = int(input("Enter second number :"))
+first = int(input("Enter first number :"))
+second = int(input("Enter second number :"))
 
-# print("Sum of 2 numbers :", first + second)
+print("Sum of 2 numbers :", first + second)
 
-# #write a program to input a side of a square and prints its area
+#write a program to input a side of a square and prints its area
 
-# side = float(input("Enter a side of a square :"))
-# area = side * side
-# print("Area of the square :", area)
+side = float(input("Enter a side of a square :"))
+area = side * side
+print("Area of the square :", area)
 
-# #Write a program to input to floating numbers and get average of them
+#Write a program to input to floating numbers and get average of them
 
-# input11 = float(input("Enter a first float value: "))
-# input22 = float(input("Enter a second float value:"))
-# average = (input11 + input22) / 2.0
-# print("The average of the two following float numbers is :", average)
+input11 = float(input("Enter a first float value: "))
+input22 = float(input("Enter a second float value:"))
+average = (input11 + input22) / 2.0
+print("The average of the two following float numbers is :", average)
 
 #Write a program to take two int numbers a and b and print true if a is greator then or equal to b if not print false
 
 a = int(input("Enter first integer: "))
 b = int(input("Enter second integer: "))
+
 
 print(a >= b)
